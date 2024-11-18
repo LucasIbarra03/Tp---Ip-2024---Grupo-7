2d0c69100ad244ee65a18ae1dccc7286ebfd4ac0
LucasIbarra03 <lucasgibarra2003@gmail.com>
2024-11-18 19:00:22 -0300

    Modificaciones de services.py
    Modificaciones para hacer funcionar la función: def getAllimages

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -3,15 +3,42 @@
 from ..persistence import repositories
 from ..utilities import translator
 from django.contrib.auth import get_user
-
+import requests 
 def getAllImages(input=None):
-    # obtiene un listado de datos "crudos" desde la API, usando a transport.py.
-    json_collection = []
+    url = "https://rickandmortyapi.com/api/character/" # URL de la API de Rick & Morty para obtener personajes
 
-    # recorre cada dato crudo de la colección anterior, lo convierte en una Card y lo agrega a images.
-    images = []
+    response = requests.get(url)  # Realizamos la solicitud GET a la API
 
-    return images
+    if response.status_code == 200: # Verificamos si la solicitud fue exitosa
+        data = response.json()  # Convertimos la respuesta en formato JSON
+
+        images = []
+        for character in data['results']: # Iteramos sobre la lista de personajes en la respuesta de la API
+            episode_url = character['episode'][0] # Obtenemos la URL del primer episodio del personaje
+            
+            # Realizamos una solicitud adicional para obtener los datos del episodio
+            episode_response = requests.get(episode_url)
+            episode_data = episode_response.json()
+            
+            # Obtenemos el nombre del episodio inicial
+            episode_name = episode_data['name']  
+
+            # Obtenemos la última ubicación del personaje
+            last_location = character['location']['name']
+
+            # Agregamos la informacion del personaje
+            character_data = {
+                'name': character['name'],  # Nombre del personaje
+                'image': character['image'],  # URL de la imagen
+                'status': character['status'],  # Estado del personaje (Alive, Dead, etc.)
+                'last_location': last_location,  # Última ubicación conocida
+                'episode_name': episode_name,  # Nombre del primer episodio donde aparece
+            }
+            
+            # Agregamos el personaje a la lista
+            images.append(character_data)
+
+        return images
 
 # añadir favoritos (usado desde el template 'home.html')
 def saveFavourite(request):
